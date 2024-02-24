423a590daa74a76b0730b37fb21edcbd9ed983ff
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-24 09:51:27 +0200

    finish task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# Installing flask from pip3.
+
+package { 'flask':
+  ensure   => '2.1.0', # Version must be 2.1.0
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
