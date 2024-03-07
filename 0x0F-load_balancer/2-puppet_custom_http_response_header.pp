778f3d8accb4d6cafa0505c8b46d679f3d1acb1b
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-03-07 02:05:04 +0200

    final work

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,9 @@
-# Installs a Nginx server with custom HTTP header
-
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
-}
-
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
-
-exec { 'add_header':
-  provider => shell,
-  command  => 'sudo sed -i "/server_name _;/a \\n\tadd_header X-Served-By $hostname;" /etc/nginx/sites-available/default',
-  before   => Exec['restart Nginx'],
-}
-
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+#!/usr/bin/env bash
+#task advance with pupppet
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
 }
