1d2715f7b443bf0b1fbf99f29fc30e27a988fb2e
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-04-05 04:00:35 +0200

    final

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-"""Write a Python script that, using this REST API, 
-for a given employee ID, 
+"""Write a Python script that, using this REST API,
+for a given employee ID,
 returns information about his/her TODO list progress."""
 
-import urllib, requests, sys
+import urllib
+import requests
+import sys
 
 
 if __name__ == "__main__":
