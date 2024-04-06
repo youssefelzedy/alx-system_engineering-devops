8520f1995fe0d3f69e23be47ab6aafee5810caf0
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-04-06 19:32:14 +0200

    final

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,18 +14,18 @@ if __name__ == "__main__":
     res1 = get('https://jsonplaceholder.typicode.com/todos/')
     json_data1 = res1.json()
     usr_id = int(argv[1])
-    
+
     res2 = get('https://jsonplaceholder.typicode.com/users')
     json_data2 = res2.json()
     name = [res for res in json_data2 if res["id"] == usr_id]
     name = name[0]["name"]
-    
+
     todo_data_filtered_before = [
         res for res in json_data1 if res["userId"] == usr_id]
     todo_data_filtered_after = [
         res for res in json_data1
         if res["userId"] == usr_id and res["completed"]]
-    
+
     print(f"Employee {name} is done with tasks("
           f"{len(todo_data_filtered_after)}/"
           f"{len(todo_data_filtered_before)}):")
