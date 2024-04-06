3c950f66f57f28b4a1b5c51da9913e35e53e28fb
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-04-06 19:39:34 +0200

    final

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Python script that, using a REST API"""
+
+"""Write a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress."""
+
 import json
 from requests import get
 from sys import argv
@@ -7,20 +11,20 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    response1 = get('https://jsonplaceholder.typicode.com/todos/')
-    todo_data = response1.json()
-    user_id = int(argv[1])
+    res1 = get('https://jsonplaceholder.typicode.com/todos/')
+    json_data1 = res1.json()
+    usr_id = int(argv[1])
 
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-    name = [res for res in data2 if res["id"] == user_id]
+    res2 = get('https://jsonplaceholder.typicode.com/users')
+    json_data2 = res2.json()
+    name = [res for res in json_data2 if res["id"] == usr_id]
     name = name[0]["name"]
 
     todo_data_filtered_before = [
-        res for res in todo_data if res["userId"] == user_id]
+        res for res in json_data1 if res["userId"] == usr_id]
     todo_data_filtered_after = [
-        res for res in todo_data
-        if res["userId"] == user_id and res["completed"]]
+        res for res in json_data1
+        if res["userId"] == usr_id and res["completed"]]
 
     print(f"Employee {name} is done with tasks("
           f"{len(todo_data_filtered_after)}/"
