657602ecfe06836ccd096d2a5045572fcc293a85
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-04-12 15:25:24 +0200

    final

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+Write a function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given, the
+function should return 0.
 """
 
 
